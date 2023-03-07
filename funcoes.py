d0cc0268edf3e872ddd956a9ca748667dad131f8
Eduardosmb <you@example.com>
2023-03-07 16:15:27 -0300

    funcao para_one_hot_encoding

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,13 @@
-from numpy import *
+import numpy as np
+def para_one_hot(palavra):
+    alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+    matriz = np.zeros((len(alfabeto), len(palavra)))
+    palavra = palavra.lower()
+    for i in range(len(palavra)):
+        letra = palavra[i]
+        indice = alfabeto.index(letra)
+        matriz[indice][i] = 1
+    return matriz
+# print(para_one_hot('bananazz'))
+
+
