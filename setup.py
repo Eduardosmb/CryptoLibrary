f6325e6f41f98da89c8bea8e2005b21fb19dcb13
Enzo <enzoq@al.insper.edu.br>
2023-03-12 13:44:56 -0300

    pip

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
  
 setup(
   name='cryptolibrary',
-  version='0.0.1',
+  version='0.0.2',
   description='Cryptography libary',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='',  
